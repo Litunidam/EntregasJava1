e345078e37d1515e9eea53ef018946e7d42fd82d
Manuel <manuemarb@gmail.com>
2020-01-23 18:40:37 +0100

    Ejercicio1amedias

--- src/entregas/Ejercicio1.java
+++ src/entregas/Ejercicio1.java
@@ -1,5 +1,26 @@
 package entregas;
+import java.util.Scanner;
+
 
 public class Ejercicio1 {
 
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		
+		int n;
+		
+		System.out.println("Introduzca la dimensión del diccionario");
+		
+		n=s.nextInt();
+		String [] diccionario = new String [n];
+		
+		for (int i=0;i<n; i++) {
+			diccionario[i]=s.nextLine();
+		}
+		  
+		
+		
+		s.close();
+	}
 }
